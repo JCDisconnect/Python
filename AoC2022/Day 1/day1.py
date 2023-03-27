925c41cb36698f36c428f6e7e855845f1618cf13
JC D <jcdcode@gmail.com>
2023-03-28 00:52:57 +0200

    Initial solution

---
+++ AoC2022/Day 1/day1.py
@@ -0,0 +1,18 @@
+"""
+https://adventofcode.com/2022/day/1
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+"""
+snackList = ["1000","2000","3000","","4000","","5000","6000","","7000","8000","9000","","10000"]
+caloriesPerElf = {}
+calories = 0
+elfCounter = 1;
+for snack in snackList:
+    if snack.isnumeric():
+        calories += int(snack)
+    else:
+        caloriesPerElf[elfCounter] = calories
+        calories = 0
+        elfCounter += 1
+
+sortedCaloriesPerElf = sorted(caloriesPerElf.items(), key=lambda x:x[1])
+print(sortedCaloriesPerElf.pop())
