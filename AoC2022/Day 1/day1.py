15fef59aefb0b990217a6c470c2bb6cb4cacd02b
JC D <jcdcode@gmail.com>
2023-03-28 16:02:07 +0200

    changed for part1&2

--- AoC2022/Day 1/day1.py
+++ AoC2022/Day 1/day1.py
@@ -19,18 +19,30 @@ snacksString = """1000
 
 snackList = snacksString.splitlines()
 
-caloriesPerElf = {}
-calories = 0
-elfCounter = 1;
-for snack in snackList:
-    if snack.isnumeric():
-        calories += int(snack)
-    else:
-        caloriesPerElf[elfCounter] = calories
-        calories = 0
-        elfCounter += 1
+def solve_puzzle(puzzle_input):
+    caloriesPerElf = {}
+    calories = 0
+    elfCounter = 1
+    for snack in snackList:
+        if snack.isnumeric():
+            calories += int(snack)
+        else:
+            caloriesPerElf[elfCounter] = calories
+            calories = 0
+            elfCounter += 1
+    #Sort list by values to get the largest value first because I'm a lazy elf
+    return sorted(caloriesPerElf.items(), key=lambda x:x[1])
 
-#Sort list by values to get the largest value first because I'm a lazy elf
-sortedCaloriesPerElf = sorted(caloriesPerElf.items(), key=lambda x:x[1])
-#Pop the first entry to get key (Elf) and his calories (value)
-print(sortedCaloriesPerElf.pop())
+def solve_part1(puzzle_input):
+    sortedCaloriesPerElf = solve_puzzle(puzzle_input)
+    print("Part 1:",sortedCaloriesPerElf.pop()[1])
+    
+def solve_part2(puzzle_input):
+    sortedCaloriesPerElf = solve_puzzle(puzzle_input)
+    sumTopThreeCalories = 0
+    for i in range(3):
+        sumTopThreeCalories += sortedCaloriesPerElf.pop()[1]
+    print("Part 2:", sumTopThreeCalories)
+
+solve_part1(snackList)
+solve_part2(snackList)
